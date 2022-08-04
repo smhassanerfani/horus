b9633fa65e047b56a9be933805239dd441080216
Mohammad Erfani <serfani@email.sc.edu>
2022-08-04 13:06:22 -0400

    Added DL model train test frameworks

---
+++ machine_learning/dataloader.py
@@ -0,0 +1,79 @@
+import os
+import numpy as np
+import torch
+from torch.utils import data
+from PIL import Image
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+
+class MaskToTensor(object):
+    def __call__(self, img):
+        return torch.from_numpy(np.array(img, dtype=np.int32)).long()
+
+
+class Materials(data.Dataset):
+
+    def __init__(self, root, split, joint_transform=None):
+        super(Materials, self).__init__()
+        self.root = root
+        self.split = split
+        self.images_base = os.path.join(self.root, "images", self.split)
+        self.masks_base = os.path.join(self.root, "masks", self.split)
+        self.items_list = self.get_images_list(self.images_base, self.masks_base)
+
+        self.joint_transform = joint_transform
+        mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        image_transforms_list = [ToTensor(), Normalize(*mean_std)]
+        self.image_transforms = Compose(image_transforms_list)
+        self.label_transforms = MaskToTensor()
+
+
+    def get_images_list(self, images_base, masks_base):
+        items_list = []
+        for root, dirs, files in os.walk(images_base, topdown=True):
+            mask_root = os.path.join(masks_base, os.path.split(root)[1])
+            for name in files:
+                if name.endswith(".jpg"):
+                    mask_name = name.replace(".jpg", ".png")
+                    img_file = os.path.join(root, name)
+                    lbl_file = os.path.join(mask_root, mask_name)
+                    items_list.append({
+                        "image": img_file,
+                        "label": lbl_file,
+                        "name": name
+                    })
+        return items_list
+
+    def __getitem__(self, index):
+        image_path = self.items_list[index]["image"]
+        label_path = self.items_list[index]["label"]
+        name = self.items_list[index]["name"]
+        image = Image.open(image_path).convert("RGB")
+        width, height = image.size
+        label = Image.open(label_path)
+
+        if self.joint_transform:
+            image, label = self.joint_transform(image, label)
+        image = self.image_transforms(image)
+        label = self.label_transforms(label)
+
+        label_copy = label - 1
+        label_copy[(label_copy==-1) | (label_copy==1) | (label_copy==2)] = 0
+        label_copy[label_copy==4] = 1
+
+        return image, label_copy, name, width, height
+
+    def __len__(self):
+        return len(self.items_list)
+
+
+def main():
+    dataset = Materials("./dataset", "val")
+    print(len(dataset))
+    dataiter = iter(dataset)
+    image, mask, name, width, height = next(dataiter)
+    print(image.shape, mask.shape, name, (height, width))
+
+
+if __name__ == "__main__":
+    main()
---
+++ machine_learning/test.py
@@ -0,0 +1,106 @@
+import os
+import argparse
+import numpy as np
+# from skimage.io import imsave
+from PIL import Image
+from tqdm import tqdm
+import torch
+import torch.backends.cudnn as cudnn
+from utils.palette import colorize_mask
+from models.pspnet import PSPNet
+from dataloader import Materials
+from torch.utils.data import DataLoader
+
+
+def main(args):
+    cudnn.enabled = True
+    cudnn.benchmark = True
+
+    if args.model == "PSPNet":
+        model = PSPNet(img_channel=3, num_classes=args.num_classes)
+
+    model.eval()
+    model.cuda()
+
+    try:
+        os.makedirs(args.save_path)
+    except FileExistsError:
+        pass
+
+    saved_state_dict = torch.load(args.restore_from)
+    model.load_state_dict(saved_state_dict)
+
+    test_dataset = Materials(args.data_directory, split="test")
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
+                                 num_workers=args.num_workers, pin_memory=True, drop_last=False)
+
+    interpolation = torch.nn.Upsample(size=(args.padding_size, args.padding_size), mode="bilinear",
+                                      align_corners=True)
+    with torch.no_grad():
+        for image, mask, name, width, height in tqdm(test_dataloader):
+
+            # GPU deployment
+            image = image.cuda()
+
+            # Compute prediction and loss
+            _, pred = model(image)
+
+            pred = interpolation(pred).squeeze(1).detach().cpu().numpy()[0]
+
+            pred[pred>0.5]=1
+            pred[pred<=0.5]=0
+
+            # pred = np.array(np.argmax(pred, axis=2), dtype=np.uint8)
+            mask = np.array(mask.squeeze(0), dtype=np.uint8)
+
+            uint8_pred = Image.fromarray(np.uint8(pred))
+            rgb_pred = colorize_mask(pred, args.num_classes)
+            rgb_mask = colorize_mask(mask, args.num_classes)
+
+            uint8_pred.save('%s/%s.png' % (args.save_path, name[0][:-4]))
+            # imsave('%s/%s.png' % (args.save_path, name[0][:-4]), pred)
+
+            if args.split != "test":
+                rgb_pred.save('%s/%s_color.png' % (args.save_path, name[0][:-4]))
+                rgb_mask.save('%s/%s_gt.png' % (args.save_path, name[0][:-4]))
+
+        print("finish")
+
+
+def get_arguments(
+    model="PSPNet",
+    split="val",
+    num_classes=1,
+    padding_size=448,
+    batch_size=1,
+    num_workers=1,
+    data_directory="./dataset",
+    restore_from="./models/results/epoch30.pth",
+    save_path="./models/results/epoch30"
+):
+    parser = argparse.ArgumentParser(description=f"Testing {model} on ATLANTIS 'test' set.")
+    parser.add_argument("--model", type=str, default=model,
+                        help=f"Model name: {model}.")
+    parser.add_argument("--split", type=str, default=split,
+                        help="ATLANTIS 'test' set.")
+    parser.add_argument("--num-classes", type=int, default=num_classes,
+                        help="Number of classes to predict, excluding background.")
+    parser.add_argument("--padding-size", type=int, default=padding_size,
+                        help="Integer number determining the height and width of model output.")
+    parser.add_argument("--batch-size", type=int, default=batch_size,
+                        help="Number of images sent to the network in one step.")
+    parser.add_argument("--num-workers", type=int, default=num_workers,
+                        help="Number of workers for multithread data loading.")
+    parser.add_argument("--data-directory", type=str, default=data_directory,
+                        help="Path to the directory containing the source dataset.")
+    parser.add_argument("--restore-from", type=str, default=restore_from,
+                        help="Where model restores parameters from.")
+    parser.add_argument("--save-path", type=str, default=save_path,
+                        help="Path to save results.")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    print(f"{args.model} is deployed on {torch.cuda.get_device_name(0)}")
+    main(args)
