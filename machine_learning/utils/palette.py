56d891405fc35ea8ff49538a335fcc9cc91673e2
smhassanerfani <serfani@email.sc.edu>
2022-08-19 14:09:57 -0400

    added generate_palette function

--- machine_learning/utils/palette.py
+++ machine_learning/utils/palette.py
@@ -2,25 +2,31 @@ import json
 import numpy as np
 from PIL import Image
 
-with open("./utils/labels_info.json", 'r') as jf:
-    dataset = json.load(jf)
 
-palette = []
-for label in dataset:
-    palette.append(label["color"][0])
-    palette.append(label["color"][1])
-    palette.append(label["color"][2])
+def generate_palette(json_path="./utils/labels_info.json"):
+    with open(json_path, 'r') as jf:
+        dataset = json.load(jf)
 
-zero_pad = 256 * 3 - len(palette)
-for i in range(zero_pad):
-    palette.append(0)
+    palette = []
+    for label in dataset:
+        palette.append(label["color"][0])
+        palette.append(label["color"][1])
+        palette.append(label["color"][2])
+
+    zero_pad = 256 * 3 - len(palette)
+    for i in range(zero_pad):
+        palette.append(0)
+
+    return palette
+
 
 def colorize_mask(mask, padding_size=False):
 
+    palette = generate_palette()
     new_mask = Image.fromarray(mask.astype(np.uint8)).convert('P')
 
     if padding_size:
-        h, w =  padding_size
+        h, w = padding_size
         new_mask = new_mask.resize((w, h), Image.NEAREST)
 
     new_mask.putpalette(palette)
