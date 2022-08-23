07cfc006263efe40c6590b682afb325560fabdfd
smhassanerfani <serfani@email.sc.edu>
2022-08-23 10:27:48 -0400

    Modified fucntions for point cloud projection on image plane

--- spatial_resection/spatial_resection.py
+++ spatial_resection/spatial_resection.py
@@ -1,68 +1,80 @@
+import os
 import cv2
 import numpy as np
-from utils import aruco_3D_to_dict, find_aruco_markers, plot_aruco_markers, load_coefficients
+import argparse
+from utils import aruco_3D_to_dict, pair_coordinates, spatial_resection, perspective_projection
 
-def pair_coordinates(image, aruco_3D_coordinates, plot=False):
-    bboxs, ids = find_aruco_markers(image, marker_size=5, total_markers=250, draw=True)
-
-    if plot:
-        plot_aruco_markers(image, bboxs, ids)
-
-    ids = np.squeeze(ids)
-    img_pts = []
-    obj_pts = []
-
-    for idx, (id, bbox) in enumerate(zip(ids, bboxs)):
-        img_pts.append([bbox[0][0][0], bbox[0][0][1]])
-        obj_pts.append(aruco_3D_coordinates[int(id)])
-
-    return ids, np.array(img_pts, dtype=np.float32), np.array(obj_pts, dtype=np.float32)
-
-def spatial_resection(camera_properties, image_points, object_points):    
-    camera_matrix, dist_coeffs, _, _ = load_coefficients(camera_properties)
-
-    success, rotation_vector, translation_vector = cv2.solvePnP(object_points, image_points, camera_matrix, dist_coeffs, cv2.SOLVEPNP_ITERATIVE)
-
-    print(f"Rotation Vector:\n {rotation_vector}")
-    print(f"Translation Vector:\n {translation_vector}")
-    return rotation_vector, translation_vector
-
-def perspective_projection(point_cloud, rvec, tvec, camera_matrix, dist_coeffs):
-    """Projects 3D points to an image plane.
-    Parameters
-    ----------
-    point_cloud: np.ndarray
-        Transformed and sub-sampled point cloud of shape (N x 3).
-    rvec: np.ndarray
-        The rotation vector which is estimated by `spatial resection`.
-    tvec: np.ndarray
-        The translation vector which is estimated by `spatial resection`.
-    camera_matrix: np.ndarray
-        Camera intrinsic matrix. 
-    dist_coeffs: np.ndarray
-        Input vector of distortion coefficients.
+def get_arguments(
+        images_path="../machine_learning/dataset/deployment/2022-08-19/images",
+        camera_config_path="./camera_config_beena.yml",
+        aruco_coordinates_path="./aruco_markers_3D.txt",
+        point_cloud3D="./total_levee_gbc.xyz",
+        save_path="../machine_learning/results/deployment/2022-08-19",
+        plot_permission=True
+        ):
     
-    Returns
-    -------
-    projected_points: np.ndarray
-        Output array of image points of shape (N x 2).
-    """
-    
-    point_cloud2D = []
-    for coordinate in point_cloud:
-        (point2D, jacobian) = cv2.projectPoints(np.array(coordinate), rvec, tvec, camera_matrix, dist_coeffs)
-        point_cloud2D.append(point2D)
+    parser = argparse.ArgumentParser(description=f"Spatial resection on captured images.")
+    parser.add_argument("--images-path", type=str, default=images_path,
+                        help="Path to the file of the captured image.")
+    parser.add_argument("--camera-config-path", type=str, default=camera_config_path,
+                        help="Path to the .yml file of the camera config.")
+    parser.add_argument("--aruco-coordinates-path", type=str, default=aruco_coordinates_path,
+                        help="Path to the .txt file of the 3D aruco markers coordinates.")
+    parser.add_argument("--point-cloud3D", type=str, default=point_cloud3D,
+                        help="Path to the .xyz file of the 3D point cloud.")
+    parser.add_argument("--save-path", type=str, default=save_path,
+                        help="Path to save results.")
+    parser.add_argument("--plot-permission", default=plot_permission, action="store_true",
+                        help="Number of classes to predict, excluding background.")
+    return parser.parse_args()
 
-    return np.array(np.squeeze(point_cloud2D))
 
-def main(image_path, aruco_coordinates_path, camera_config_path):
-    aruco_3D_dict = aruco_3D_to_dict(aruco_coordinates_path)
-    image = cv2.imread(image_path)
-    ids, image_points, object_points = pair_coordinates(image, aruco_3D_dict, plot=False)
-    
-    if len(ids) >= 6:
-        camera_properties = camera_config_path
-        spatial_resection(camera_properties, image_points, object_points)
+def main(args):
+
+    for root, dir, files in os.walk(args.images_path, topdown=True):
+        for file in files:
+            if file.endswith(".jpg"):
+
+                image = cv2.imread(os.path.join(root, file))
+                aruco_3D_dict = aruco_3D_to_dict(args.aruco_coordinates_path)
+
+                ids, image_points, object_points = pair_coordinates(image, aruco_3D_dict,
+                                                                    plot=args.plot_permission, save_path=args.save_path)
+
+                if len(ids) >= 6:
+
+                    camera_matrix, dist_coeffs, rvec, tvec = spatial_resection(args.camera_config_path,
+                                                                               image_points, object_points)
+
+                    point_cloud3D = np.loadtxt(args.point_cloud3D, delimiter=",")
+
+                    if point_cloud3D.shape[1] > 3:
+                        point_cloud3D = point_cloud3D[:, :3]
+                    
+                    point_cloud2D = perspective_projection(point_cloud3D, rvec, tvec, camera_matrix, dist_coeffs)
+
+                    save_path = os.path.join(args.save_path, f'{file.split(".")[0]}.pts')
+                    np.savetxt(save_path, point_cloud2D, delimiter=',')
+
+                    if args.plot_permission:
+                        for val in point_cloud2D:
+                            cv2.putText(image, ".", (int(val[0]), int(val[1])), cv2.FONT_HERSHEY_PLAIN, 1, (75, 20, 20), 1)
+
+
+                        resized_image = cv2.resize(image, (1920, 1080))
+                        cv2.imshow("Image", resized_image)
+                        cv2.waitKey(0)
+                        cv2.destroyAllWindows()
+                        
+                        x = input('Do you want to save:')
+                        if (x.lower() == 'yes') or (x.lower() == 'y'):
+                            file_name = input('Please enter the file name:')
+                            save_path = os.path.join(args.save_path, f"{file_name}.png")
+                            cv2.imwrite(save_path, image)
+
+                    print(f"Point Cloud 2D has been created based on image: {file}")
+                    exit()
 
 if __name__ == "__main__":
-    main("./2021-12-02-1504.jpg", "./aruco_object_coordinates.txt", "./camera_config.yml")
+    args = get_arguments()
+    main(args)
--- spatial_resection/utils.py
+++ spatial_resection/utils.py
@@ -13,8 +13,6 @@ def load_coefficients(path):
     # FILE_STORAGE_READ
     cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
 
-    # note we also have to specify the type to retrieve other wise we only get a
-    # FileNode object back instead of a matrix
     camera_matrix = cv_file.getNode('K').mat()
     dist_coefs = cv_file.getNode('D').mat()
     rot_vecs = cv_file.getNode('R').mat()
@@ -28,8 +26,10 @@ def plot_aruco_markers(img, bboxs, ids):
         for bbox, id in zip(bboxs, ids):
             cv2.putText(img, str(id), (int(bbox[0][0][0]), int(bbox[0][0][1])), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
 
-    cv2.imshow("Image", img)
+    resized_img = cv2.resize(img, (1920, 1080))
+    cv2.imshow("Image", resized_img)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 def find_aruco_markers(img, marker_size=5, total_markers=50, draw=True):
@@ -45,13 +45,111 @@ def find_aruco_markers(img, marker_size=5, total_markers=50, draw=True):
 
     return bboxs, ids
 
+
+def pair_coordinates(image, aruco_3D_coordinates, plot=False, save_path="./"):
+    bboxs, ids = find_aruco_markers(image, marker_size=5, total_markers=250, draw=True)
+
+    if plot:
+        plot_aruco_markers(image, bboxs, ids)
+
+        x = input('Do you want to save:')
+        if (x.lower() == 'yes') or (x.lower() == 'y'):
+            file_name = input('Please enter the file name:')
+            save_path = os.path.join(save_path, f"{file_name}.png")
+            cv2.imwrite(save_path, image)
+
+    ids = np.squeeze(ids)
+    useless_idxs = []
+    img_pts = []
+    obj_pts = []
+
+
+    for idx, (id, bbox) in enumerate(zip(ids, bboxs)):
+        if id in aruco_3D_coordinates.keys():
+            img_pts.append([bbox[0][0][0], bbox[0][0][1]])
+            obj_pts.append(aruco_3D_coordinates[int(id)])
+        else:
+            useless_idxs.append(idx)
+
+    return np.delete(ids, useless_idxs), np.array(img_pts, dtype=np.float32), np.array(obj_pts, dtype=np.float32)
+
+def spatial_resection(camera_properties, image_points, object_points):
+    """For further information visit following website:
+    learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
+
+    Parameters
+    ----------
+    camera_properties: yml
+        Intrinsic and extrinsic parameters of the camera.
+    image_points: np.ndarray
+        2D coordinates of ArUco markers.
+    object_points: np.ndarray
+        3D locations of the same ArUco markers.
+    
+    Returns
+    -------
+    rotation_vector: np.ndarray
+        Rotation vector.
+    translation_vector: np.ndarray
+        Translation vector.
+    """    
+    
+    # Intrinsic parameters of the camera, distortion coefficients
+    camera_matrix, dist_coeffs, rvec, tvec = load_coefficients(camera_properties)
+    
+    # Initial approximations of the rotation and translation vectors
+    rvec = np.array([1.34596194, -1.64632315, 1.0735686])
+    tvec = np.array([1.54266924, 1.94512742, 4.21663091])
+
+    # The Direct Linear Transform (DLT) solution followed by Levenberg-Marquardt optimization
+    success, rotation_vector, translation_vector = cv2.solvePnP(
+        object_points,
+        image_points,
+        camera_matrix,
+        dist_coeffs,
+        rvec,
+        tvec,
+        useExtrinsicGuess=True,
+        flags=cv2.SOLVEPNP_ITERATIVE
+    )
+
+    print(f"Rotation Vector:\n {rotation_vector}")
+    print(f"Translation Vector:\n {translation_vector}")
+    return camera_matrix, dist_coeffs, rotation_vector, translation_vector
+
+def perspective_projection(point_cloud, rvec, tvec, camera_matrix, dist_coeffs):
+    """Projects 3D points to an image plane.
+    Parameters
+    ----------
+    point_cloud: np.ndarray
+        Transformed and sub-sampled point cloud of shape (N x 3).
+    rvec: np.ndarray
+        The rotation vector which is estimated by `spatial resection`.
+    tvec: np.ndarray
+        The translation vector which is estimated by `spatial resection`.
+    camera_matrix: np.ndarray
+        Camera intrinsic matrix. 
+    dist_coeffs: np.ndarray
+        Input vector of distortion coefficients.
+    
+    Returns
+    -------
+    projected_points: np.ndarray
+        Output array of image points of shape (N x 2).
+    """
+    
+    point_cloud2D = []
+    for coordinate in point_cloud:
+        (point2D, jacobian) = cv2.projectPoints(np.array(coordinate), rvec, tvec, camera_matrix, dist_coeffs)
+        point_cloud2D.append(point2D)
+
+    return np.array(np.squeeze(point_cloud2D))
+
 def main():
     cap = cv2.VideoCapture(0)
 
     while (True):
         success, img = cap.read()
-        #img = cv2.rotate(img, cv2.ROTATE_180) #cv2.ROTATE_90_COUNTERCLOCKWISE
-        #img = cv2.flip(img, 0)
         bboxs, ids = find_aruco_markers(img)
 
         if (ids is not None):
